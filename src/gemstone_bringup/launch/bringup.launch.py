1e965a1b6ab6e356761509e276828e6b987aa3c3
alitalhq <alitqlhq@gmail.com>
2026-07-29 15:01:05 +0300

    bug fix

--- src/gemstone_bringup/launch/bringup.launch.py
+++ src/gemstone_bringup/launch/bringup.launch.py
@@ -22,6 +22,7 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -55,7 +56,7 @@ def generate_launch_description():
         output='screen',
         condition=IfCondition(LaunchConfiguration('enable_state_publishers')),
         parameters=[{
-            'robot_description': Command(['xacro ', xacro_file]),
+            'robot_description': ParameterValue(Command(['xacro ', xacro_file]), value_type=str),
         }],
     )
 
